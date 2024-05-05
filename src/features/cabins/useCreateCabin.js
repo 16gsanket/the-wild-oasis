c0fd3a7b9b4e9ec97b244fa914b4f066e8325a59
16gsanket <16gsanket@gmail.com>
2024-05-05 20:37:06 +0530

    update settings feature added to v2

--- src/features/cabins/useCreateCabin.js
+++ src/features/cabins/useCreateCabin.js
@@ -2,21 +2,19 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 import { createEditCabin } from "../../services/apiCabins";
 
+export default function useCreateCabin() {
+  const queryClient = useQueryClient();
+  const { isLoading: isAddingCabin, mutate: createCabin } = useMutation({
+    mutationFn: createEditCabin,
+    onSuccess: () => {
+      toast.success("added a new cabin");
+      queryClient.invalidateQueries({
+        queryKey: ["cabin"],
+      });
+      // reset();
+    },
+    onError: (err) => toast.error(err.message),
+  });
 
-
-export default function useCreateCabin(){
-    const queryClient = useQueryClient();
-    const { isLoading: isAddingCabin, mutate:createCabin } = useMutation({
-      mutationFn: createEditCabin,
-      onSuccess: () => {
-        toast.success("added a new cabin");
-        queryClient.invalidateQueries({
-          queryKey: ["cabin"],
-        });
-        // reset();
-      },
-      onError: (err) => toast.error(err.message),
-    });
-
-    return {createCabin , isAddingCabin}
+  return { createCabin, isAddingCabin };
 }
--- src/features/settings/UpdateSettingsForm.jsx
+++ src/features/settings/UpdateSettingsForm.jsx
@@ -1,22 +1,68 @@
-import Form from '../../ui/Form';
-import FormRow from '../../ui/FormRow';
-import Input from '../../ui/Input';
+import { useForm } from "react-hook-form";
+import Form from "../../ui/Form";
+import FormRowRefactored from "../../ui/FormRowRefactored";
+import Input from "../../ui/Input";
+import Spinner from "../../ui/Spinner";
+import { useSettings } from "./useSettings";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { updateSetting } from "../../services/apiSettings";
+import toast from "react-hot-toast";
 
 function UpdateSettingsForm() {
+  const {
+    settings: { minBookingLength, maxBookingLength , maxGuestsPerBooking , breakFastPrice } = {},
+    isLoading,
+    error,
+  } = useSettings();
+
+  const {register , reset , handleSubmit} = useForm()
+  const queryClient = useQueryClient()
+
+
+  if (isLoading == false) {
+    console.log(minBookingLength, maxBookingLength);
+  }
+
+  const{isLoading : isUpdatingSettings , mutate:changeSetting} = useMutation({
+    mutationFn:updateSetting,
+    onSuccess:()=>{
+      toast.success("Settings updated successfully");
+      queryClient.invalidateQueries({
+        queryKey:['settings']
+      })
+    },
+    onError: (err)=>toast.error('err.message')
+  })
+
+  
+  
+
+
+   function onsubmit(data){
+    
+    
+    const newData = {minBookingLength: data.minnights , maxBookingLength : data.maxnights, maxGuestsPerBooking : data.maxguests , breakFastPrice : data.breakfastprice}
+      changeSetting(newData);
+      
+   }
+
+
+  if(isLoading || isUpdatingSettings) return <Spinner/>
   return (
-    <Form>
-      <FormRow label='Minimum nights/booking'>
-        <Input type='number' id='min-nights' />
-      </FormRow>
-      <FormRow label='Maximum nights/booking'>
-        <Input type='number' id='max-nights' />
-      </FormRow>
-      <FormRow label='Maximum guests/booking'>
-        <Input type='number' id='max-guests' />
-      </FormRow>
-      <FormRow label='Breakfast price'>
-        <Input type='number' id='breakfast-price' />
-      </FormRow>
+    <Form onSubmit={handleSubmit(onsubmit)}>
+      <FormRowRefactored label="Minimum nights/booking" >
+        <Input type="number" id="minnights" defaultValue={minBookingLength} {...register('minnights') }/>
+      </FormRowRefactored>
+      <FormRowRefactored label="Maximum nights/booking">
+        <Input type="number" id="maxnights" defaultValue={maxBookingLength} {...register('maxnights') }/>
+      </FormRowRefactored>
+      <FormRowRefactored label="Maximum guests/booking">
+        <Input type="number" id="maxguests" defaultValue={maxGuestsPerBooking} {...register('maxguests') }/>
+      </FormRowRefactored>
+      <FormRowRefactored label="Breakfast price">
+        <Input type="number" id="breakfastprice" defaultValue={breakFastPrice} {...register('breakfastprice') } />
+      </FormRowRefactored>
+      <button type="submit">Submit</button>
     </Form>
   );
 }
